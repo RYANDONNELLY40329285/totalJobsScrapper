4de76642d717b021c9cacc2217ba90b85d80b895
40329285 <rdonnelly49@qub.ac.uk>
2026-02-07 18:59:34 +0000

    updated file structure

--- totalJobsScrapper/jest.config.js
+++ totalJobsScrapper/jest.config.js
@@ -1,12 +1,15 @@
 export default {
   testEnvironment: "node",
+
   collectCoverage: true,
   collectCoverageFrom: [
     "src/**/*.js",
-    "!src/api/server.js", 
+    "!src/api/server.js",
   ],
+
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"],
+
   coverageThreshold: {
     global: {
       branches: 70,
